e59652ed778a3638f9a3374490ba24a0745519eb
Doug Jones <DSJR1284@GMAIL.COM>
2021-08-09 19:20:02 -0400

    pod commit

---
+++ my_cards_api/db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# Category.create(sport: "football")
+
+# Category.create(sport: "basketball")
+
+# Category.create(sport: "baseball")
+
+# Category.create(sport: "hockey")
+
+
+#  Card.create(name: "Micheal Vick", image: "https://www.biography.com/.image/t_share/MTIwNjA4NjMzNjA2MzQyMTU2/michael-vick-241100-1-402.jpg", category_id: 1, team: "Atlanta Falcons")
+
+#  Card.create(name: "Derrick Rose", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4WUS4sy2rM7nHdEG18d1qZ-gWAoaA2PXipQ&usqp=CAU", category_id: 2, team: "Chicago Bulls")
