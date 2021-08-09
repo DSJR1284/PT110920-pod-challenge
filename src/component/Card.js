e643ac39e57ea05a0ddfabc7392225c80777c90f
Sam Bequette <sbb3391@gmail.com>
2021-08-09 19:52:48 -0400

    adds fetch to Card.js

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,9 +3,23 @@
 import React, { Component } from 'react'
 
 export default class Card extends Component {
+
+    state = [
+
+    ]
+
+    componentDidMount() {
+        fetch(`http://localhost:3000/cards`)
+        .then(resp => resp.json())
+        .then(json => {
+            
+        })
+    }
+
     render() {
         return (
             <div>
+                <h1></h1>
                 
             </div>
         )
